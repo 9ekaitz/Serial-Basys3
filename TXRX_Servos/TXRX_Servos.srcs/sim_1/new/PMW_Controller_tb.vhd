09199a76f022f4b312702f390cc09fdea13d06dd
auribesalgo <ainhize.uribesalgo@alumni.mondragon.edu>
2021-01-26 15:18:08 +0100

    simulaciones creadas, falta crear el process de las simulaciones

---
+++ TXRX_Servos/TXRX_Servos.srcs/sim_1/new/PMW_Controller_tb.vhd
@@ -0,0 +1,81 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 26.01.2021 15:10:43
+-- Design Name: 
+-- Module Name: PMW_Controller_tb - Behavioral
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity PMW_Controller_tb is
+--  Port ( );
+end PMW_Controller_tb;
+
+architecture Behavioral of PMW_Controller_tb is
+
+component PWM_controller
+    Port ( clk : in STD_LOGIC;
+           clk_pmw : in STD_LOGIC;
+           angle_byte : in STD_LOGIC_VECTOR (7 downto 0);
+           pmw : out STD_LOGIC);
+end component;
+
+signal clk, clk_pmw: std_logic;
+signal angle_byte: std_logic_vector(7 downto 0);
+signal pmw: std_logic;
+
+begin
+
+dut: PWM_controller port map( clk => clk,
+                              clk_pmw => clk_pmw,
+                              angle_byte => angle_byte,
+                              pmw => pmw);
+                             
+clk_proc: process
+begin
+clk<='1';
+wait for 5 ns;
+clk<='0';
+wait for 5 ns;
+end process;
+
+stim_proc: process
+begin
+rst<='1';
+wait for 10 ns;
+rst<='0';
+
+wait for 5000 ns;
+
+wait for 5000 ns;
+
+wait;
+
+end process;              
+                              
+
+end Behavioral;
--- TXRX_Servos/TXRX_Servos.srcs/sim_1/new/data_memoria_tb.vhd
+++ TXRX_Servos/TXRX_Servos.srcs/sim_1/new/data_memoria_tb.vhd
@@ -52,7 +52,44 @@ component Data_memoria
              id_out : out STD_LOGIC_VECTOR (7 downto 0));
 end component;
 
+signal clk,ena,reset,data_receive: std_logic;
+signal led_send,led_angle,led_id,led_idle,led_off: std_logic;
+siganl data_in, angle_out,id_out: std_logic_vector(7 downto 0);
+
 begin
 
+dut: Data_memoria port map ( clk =>clk,
+                             ena => ena,
+                             reset => reset,
+                             data_in => data_in,
+                             data_receive => data_receive,
+                             led_send => led_send,
+                             led_angle => led_angle,
+                             led_id => led_id,
+                             led_idle => led_idle,
+                             led_off => led_off,
+                             angle_out => angle_out,
+                             id_out => id_out);
+clk_proc: process
+begin
+clk<='1';
+wait for 5 ns;
+clk<='0';
+wait for 5 ns;
+end process;
+
+stim_proc: process
+begin
+rst<='1';
+wait for 10 ns;
+rst<='0';
+
+wait for 5000 ns;
+
+wait for 5000 ns;
+
+wait;
+
+end process;
 
 end Behavioral;
